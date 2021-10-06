f9b6880e0e84f66f1e8116a89ce15fbddd59bd2c
artem <vrail30@outlook.com>
2021-10-06 18:34:21 +0300

    Remove example from function description

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ def get_fractions(a_b: str, c_b: str) -> str:
     """
     Create a function what takes two parameters of string type which are fractions and
     returns a sum expression of these fractions and the sum result.
-    For example:
-    >>> a_b = '1/3'
-    >>> c_b = '5/3'
-    >>> get_fractions(a_b, c_b)
-    '1/3 + 5/3 = 6/3'
     """
     return (
         a_b
